97cdddb0ea1d9511668878b2fa088023cdf34cb2
aarora8 <aarora8@jhu.edu>
2020-12-30 10:54:54 -0500

    adding comments

--- code/data.py
+++ code/data.py
@@ -6,7 +6,7 @@ import re
 import numpy as np
 from collections import Counter
 from kaldi_io import read_mat_scp
-
+import logging
 
 class Dataset(object):
     """Creat data class."""
@@ -25,7 +25,7 @@ class Dataset(object):
 
         word2id = {v: k for k, v in enumerate(uwords)}
         ids = [word2id[w] for w in words]
-
+        # ids contain word to id mapping, ids size = num examples
         if feature_mean is None:
             feature_mean, n = 0.0, 0
             for x in data:
@@ -34,12 +34,18 @@ class Dataset(object):
             feature_mean /= n
         self.feature_mean = feature_mean
 
-        self.data = np.array([x - self.feature_mean for x in data])
+        
+        self.data = np.array([x - self.feature_mean for x in data], dtype=object)
         self.ids = np.array(ids, dtype=np.int32)
         self.id_counts = Counter(ids)
-
+        # id_counts is how many times each word occur
+        print(self.id_counts)
         self.num_classes = len(self.id_counts)
         self.num_examples = len(self.ids)
+        logger = logging.getLogger()
+        #logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+        logging.error("System reported: %s", self.ids)
+        print(uwords)
 
     def shuffle(self):
         """Shuffle data."""
@@ -51,26 +57,43 @@ class Dataset(object):
 
     def pad_features(self, indices):
         """Pad acoustic features to max length sequence."""
+        #indices contain all utteranceid in a mini batch
+        #same utterances and different utterances 
         b = len(indices)
         lens = np.array([len(xx) for xx in self.data[indices]], dtype=np.int32)
+        print("lens same ",lens)
         padded = np.zeros((b, max(lens), self.feature_dim))
         for i, (x, l) in enumerate(zip(self.data[indices], lens)):
             padded[i, :l] = x
-
+        # padded is the features
+        # lens is the list of length of the features
         return padded, lens, self.ids[indices]
 
 
     def batch(self, batch_size, max_same=1, max_diff=1):
         """Batch data."""
-
+        # len(same) = 224 = 1*32 + max_same*32 + max_diff*32 = 32 + 32 + 160
+        # len(diff) = 160 = max_diff*32
         self.shuffle()
 
         same = []
         for index, word_id in enumerate(self.ids):  # collect same samples
+            # for each word find the same word
+            # the size of same = num_examples 
+            # for each index, it stores the index of same word
+            # shape same  (10959, 1), shape diff  (10959, 5), type diff  <class 'numpy.ndarray'>
+            # value diff  [[ 5793  1052   638  7449  3084]
+            # [ 4564  3630 10646  4874  6940]
             indices = np.where(self.ids == word_id)[0]
             same.append(np.random.permutation(indices[indices != index])[:max_same])
+            #print("len same ",len(same))
+            #print("value same ",same[-1])
         same = np.array(same)
-
+        #print("num examples",self.num_examples)
+        print("value same ",same)
+        #print("type same ",type(same))
+        #print("len same ",len(same))
+        #print("shape same ",same.shape)
         diff_ids = np.random.randint(0, self.num_classes - 1, (self.num_examples, max_diff))
         diff_ids[diff_ids >= np.tile(self.ids.reshape(-1, 1), [1, max_diff])] += 1
 
@@ -79,22 +102,31 @@ class Dataset(object):
             indices = np.where(diff_ids == word_id)
             diff[indices] = np.where(self.ids == word_id)[0][np.random.randint(0, count, len(indices[0]))]
 
+        #print("value diff ",diff)
+        #print("type diff ",type(diff))
+        #print("len diff ",len(diff))
+        #print("shape diff ",diff.shape)
         get_batch_indices = lambda start: range(start, min(start + batch_size, self.num_examples))
 
         for indices in map(get_batch_indices, range(0, self.num_examples, batch_size)):
 
             if self.is_train:
                 b = len(indices)
-
+                #print(indices) range(9760, 9792), 32 indices same as batchsize
+                # np.arange(5) array([0, 1, 2, 3, 4])
                 same_partition = [np.arange(b)]  # same segment ids for anchors
                 same_partition += [(b + i) * np.ones(len(x)) for i, x in enumerate(same[indices])]  # same segment ids for same examples
                 same_partition += [(2 * b) + np.arange(max_diff * b)]  # same segment ids for diff examples
                 same_partition = np.concatenate(same_partition)
 
                 diff_partition = np.concatenate([i * np.ones(max_diff) for i in range(b)])  # diff segment ids for diff examples
-
+                print("len same ", len(same_partition))
+                print("len same ", len(diff_partition))
+                #cat1 = np.hstack(same[indices])   32
+                #cat2 = diff[indices].flatten()    160
+                #indices1 = np.concatenate((indices, cat1, cat2))   32 + 32 + 160
                 indices = np.concatenate((indices, np.hstack(same[indices]), diff[indices].flatten()))
-
+                # data is paded data   
                 data, lens, _ = self.pad_features(indices)
                 yield data, lens, same_partition, diff_partition
 
--- code/main.py
+++ code/main.py
@@ -1,13 +1,10 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-# --
 from os import path, makedirs
 import numpy as np
-import tensorflow as tf
 from model import Model
 from data import Dataset
-from average_precision import average_precision
 
 class Config(object):
     """Set up model for debugging."""
@@ -41,45 +38,59 @@ def main():
     config = Config()
 
     train_data = Dataset(partition="train", config=config)
-    dev_data = Dataset(partition="dev", config=config, feature_mean=train_data.feature_mean)
-
-    train_model = Model(is_train=True, config=config, reuse=None)
-    dev_model = Model(is_train=False, config=config, reuse=True)
-
     batch_size = config.batch_size
-
-    saver = tf.train.Saver()
-
-    proto = tf.ConfigProto(intra_op_parallelism_threads=2)
-    with tf.Session(config=proto) as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(tf.local_variables_initializer())
-
-        ckpt = tf.train.get_checkpoint_state(config.ckptdir)
-        if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            print("restored from %s" % ckpt.model_checkpoint_path)
-
-        batch = 0
-        for epoch in range(config.current_epoch, config.num_epochs):
-            print("epoch: ", epoch)
-
-            losses = []
-            for x, ts, same, diff in train_data.batch(batch_size, config.max_same, config.max_diff):
-                _, loss = train_model.get_loss(sess, x, ts, same, diff)
-                losses.append(loss)
-                if batch % config.log_interval == 0:
-                    print("avg batch loss: %.4f" % np.mean(losses[-config.log_interval:]))
-                batch += 1
-
-            embeddings, labels = [], []
-            for x, ts, ids in dev_data.batch(batch_size):
-                embeddings.append(dev_model.get_embeddings(sess, x, ts))
-                labels.append(ids)
-            embeddings, labels = np.concatenate(embeddings), np.concatenate(labels)
-            print("ap: %.4f" % average_precision(embeddings, labels))
-
-            saver.save(sess, path.join(config.ckptdir, "model"), global_step=epoch)
+    total_batch=0
+    for x, ts, same, diff in train_data.batch(batch_size, config.max_same, config.max_diff):
+        total_batch=total_batch+1
+        break
+        #len same  224
+        #len diff  160
+        #print("len same ",len(same))
+        #print("len diff ",len(diff))
+    print(len(same))
+    print(len(diff))
+    print(x.shape)
+    print(ts)
+    
+#    dev_data = Dataset(partition="dev", config=config, feature_mean=train_data.feature_mean)
+#
+#    train_model = Model(is_train=True, config=config, reuse=None)
+#    dev_model = Model(is_train=False, config=config, reuse=True)
+#
+#    batch_size = config.batch_size
+#
+#    saver = tf.train.Saver()
+#
+#    proto = tf.ConfigProto(intra_op_parallelism_threads=2)
+#    with tf.Session(config=proto) as sess:
+#        sess.run(tf.global_variables_initializer())
+#        sess.run(tf.local_variables_initializer())
+#
+#        ckpt = tf.train.get_checkpoint_state(config.ckptdir)
+#        if ckpt and ckpt.model_checkpoint_path:
+#            saver.restore(sess, ckpt.model_checkpoint_path)
+#            print("restored from %s" % ckpt.model_checkpoint_path)
+#
+#        batch = 0
+#        for epoch in range(config.current_epoch, config.num_epochs):
+#            print("epoch: ", epoch)
+#
+#            losses = []
+#            for x, ts, same, diff in train_data.batch(batch_size, config.max_same, config.max_diff):
+#                _, loss = train_model.get_loss(sess, x, ts, same, diff)
+#                losses.append(loss)
+#                if batch % config.log_interval == 0:
+#                    print("avg batch loss: %.4f" % np.mean(losses[-config.log_interval:]))
+#                batch += 1
+#
+#            embeddings, labels = [], []
+#            for x, ts, ids in dev_data.batch(batch_size):
+#                embeddings.append(dev_model.get_embeddings(sess, x, ts))
+#                labels.append(ids)
+#            embeddings, labels = np.concatenate(embeddings), np.concatenate(labels)
+#            print("ap: %.4f" % average_precision(embeddings, labels))
+#
+#            saver.save(sess, path.join(config.ckptdir, "model"), global_step=epoch)
 
 if __name__ == "__main__":
     main()
